5993fb37a91724c8677a0a84eff7b52dcf9bc716
snus-kin <tcarroll@snufk.in>
2025-09-05 10:16:30 +0100

    fix: use a submodule for the protobufs

--- __tests__/vitest.config.ts
+++ __tests__/vitest.config.ts
@@ -1,16 +1,23 @@
-import { configDefaults, coverageConfigDefaults, defineConfig } from 'vitest/config'
+import {
+  configDefaults,
+  coverageConfigDefaults,
+  defineConfig,
+} from 'vitest/config';
 
 export default defineConfig({
   test: {
-    reporters: [
-      'verbose',
-      'github-actions'
-    ],
-    exclude: [...configDefaults.exclude, 'build/**/*'],
+    reporters: ['verbose', 'github-actions'],
+    exclude: [...configDefaults.exclude, 'build/**/*', 'athena-protobufs/**/*'],
     coverage: {
       enabled: true,
       provider: 'v8',
-      exclude: [...coverageConfigDefaults.exclude, 'build/**/*','samples/**/*', 'src/athena/**/*']
+      exclude: [
+        ...coverageConfigDefaults.exclude,
+        'build/**/*',
+        'samples/**/*',
+        'src/athena/**/*',
+        'athena-protobufs/**/*',
+      ],
     },
   },
-})
+});
